71823a217eac7bbd53117f794f2169aac31718ae
Mohamed Elkhazendar <92602037+Khazendar99@users.noreply.github.com>
2024-12-07 14:10:51 +0200

    Add files via upload

--- BMW/main.js
+++ BMW/main.js
@@ -2,5 +2,5 @@ window.onload = function () {
   setTimeout(() => {
     document.getElementById("loading-screen").style.display = "none";
     document.getElementById("main-content").style.display = "block";
-  }, 3000);
+  }, 30);
 };
